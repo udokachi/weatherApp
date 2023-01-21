dcb6985ce0ddcd15ca7f66c73c868327cca3ed3c
udokachi <chidinma242@gmail.com>
2023-01-21 16:49:59 +0100

    weatherexternalapiconsumptionAndImplemetation

--- weatherapp/pages/index.js
+++ weatherapp/pages/index.js
@@ -1,11 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import axios from 'axios'
+import { useState } from 'react'
+import {BsSearch} from 'react-icons/bs'
 
 
 export default function Home() {
 
+  const [city, setCity] = useState('')
+  const [weather, setWeather] = useState({})
+  const [loading, setLoading] = useState(false)
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+
+  const fetchWeather = (e) =>{
+    e.preventDefault()
+    setLoading(false)
+    axios.get(url).then((response) => {
+      setWeather(response.data)
+      console.log(response.data)
+    })
+  }
   return (
     <div>
       <Head>
